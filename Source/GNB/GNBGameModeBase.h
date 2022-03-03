9da5bd7a0699cba73d9bd79f9ec99f1229fe676d
anyc1 <anyc1@YUCHANLAPTOP>
2022-03-03 22:11:51 +0900

    초기 커밋.

---
+++ Source/GNB/GNBGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GNBGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GNB_API AGNBGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
